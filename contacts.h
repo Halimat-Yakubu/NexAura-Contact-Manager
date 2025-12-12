687511f82e68106ab8b26a98643a466bd4e039e6
Halimat-Yakubu <halimatyakubu50@gmail.com>
2025-12-12 13:36:47 +0100

    Update contacts.h

--- contacts.h
+++ contacts.h
@@ -1,1 +1,17 @@
-e
+#ifndef CONTACTS_H
+#define CONTACTS_H
+
+extern char first_names[500][50];
+extern char last_names[500][50];
+extern char phone_numbers[500][20];
+extern char emails[500][100];
+extern int ids[500];
+extern int count;
+
+void add_contact();
+void get_contact_by_id();
+void update_contact_by_id();
+void delete_contact_by_id();
+void list_contacts();
+void save_contact_to_file();
+#endif
